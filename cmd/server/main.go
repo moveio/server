1c33f25442334cdc45022ddf59e395951a8a7978
Jakub Kulich <jakubkulich@gmail.com>
2017-04-07 15:00:20 +0200

    feat(moveio): init project

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
 )
 
 func main() {
-	fmt.Println("Something")
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
